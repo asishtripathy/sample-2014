8c2d33113805e2a79d0f193dbd784fb4a668c6e6
asishtripathy <asisht@mindfiresolutions.com>
2014-08-29 01:53:09 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapTest::Application.config.secret_token = 'ba27b81ea79203bafcfae55632186a15c81d2216bc781fd212ab3de2d0b8958a5967d5766d3d162094e145f56444b4d5ef24ab6046c9c0a7689f3c415ea82437'
